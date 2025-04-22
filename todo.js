c04857fc144c761c17d4bf49d2042d5cb7825d6e
annishasaravanan <annishasaravanan83@gmail.com>
2025-04-22 22:10:53 +0530

    update committe

---
+++ todo.js
@@ -0,0 +1,11 @@
+ 
+ function attachEventListeners() {
+    let count = 0;
+
+    document.getElementById("clickme")
+        .addEventListener("click", function xyz() {
+            console.log("Button clicked", ++count);
+        });
+}
+
+attachEventListeners();
