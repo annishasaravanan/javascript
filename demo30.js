e517dd46fb0de17744f77b15c3a697851c9b52e6
annishasaravanan <annishasaravanan83@gmail.com>
2025-05-18 10:30:16 +0530

    update

--- demo30.js
+++ demo30.js
@@ -1,4 +1,4 @@
-// const cart=["shirt","pants","shoes"];
+const cart=["shirt","pants","shoes"];
 
 // createOrder(cart,function(orderId){
 //     proceedToPayment(orderId);
@@ -11,6 +11,32 @@
 //  proceedToPayment(orderId);
 // });
 
-const GITHUB_API="https://api.github.com/annishasaravanan/js";
+// const GITHUB_API="https://api.github.com/annishasaravanan/js";
 
-const user=fetch(GITHUB_API);
+// const user =fetch(GITHUB_API);
+// console.log(user);
+
+// user.then (function(response){
+//     console.log(data);
+// })
+
+
+function addToCart(item) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (item) {
+        resolve(`${item} added to cart`);
+      } else {
+        reject("No item specified");
+      }
+    }, 2000);
+  });
+}
+
+addToCart("Shoes")
+  .then((message) => {
+    console.log("✅ Success:", message);
+  })
+  .catch((error) => {
+    console.log("❌ Error:", error);
+  });
