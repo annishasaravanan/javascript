96e2434401f4d1c2744fdf5df5257f7a16a8aa85
annishasaravanan <annishasaravanan83@gmail.com>
2025-05-27 19:03:12 +0530

    updatefile

---
+++ java1.js
@@ -0,0 +1,24 @@
+// //  console.log(a);
+// // var a = 5;
+// // console.log(a); 
+
+
+// var a=10;          
+// console.log(a);  
+// a = 5;
+// console.log(a);  
+
+// console.log(b);
+// let b = 10;
+
+// greet();
+// function greet() {
+//   console.log("Hello!");
+// }
+
+
+
+// greet(); 
+// var greet = function () {
+//   console.log("Hi!");
+// };
