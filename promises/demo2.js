9a6b6df530af85efcd85b6ba8def9accf13a9e6a
annishasaravanan <annishasaravanan83@gmail.com>
2025-06-07 11:17:26 +0530

    update code

---
+++ promises/demo2.js
@@ -0,0 +1,25 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("p1 success");
+    }, 3000);
+});
+
+const p2 = new Promise((reject) => {
+    setTimeout(() => {
+        reject("p2 fails");
+    }, 1000);
+});
+
+const p3 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("p3 success");
+    }, 2000);
+});
+
+Promise.allSettled([p1, p2, p3])
+  .then((values) => {
+    console.log(values); 
+  })
+  .catch((error) =>{
+    console.error("Error: ",error);
+  });
